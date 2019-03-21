d2d2891034d4cba2563fb975fcb503913ae45243
Konrad Eichstaedt <konrad.eichstaedt@sbb.sbk-berlin.de>
2019-03-21 09:10:06 +0100

    Remove OWASP Security Check

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -37,18 +37,7 @@ pipeline {
                 }
             }
         }
-
-        stage('Dependency Check') {
-
-            steps {
-
-                dependencyCheckAnalyzer datadir: 'dependency-check-data', includeVulnReports: true, hintsFile: '', includeCsvReports: false, includeHtmlReports: true, includeJsonReports: false, isAutoupdateDisabled: false, outdir: '', scanpath: '', skipOnScmChange: false, skipOnUpstreamChange: false, suppressionFile: '', zipExtensions: ''
-
-                dependencyCheckPublisher canComputeNew: false, defaultEncoding: '', healthy: '', pattern: '', unHealthy: ''
-
-                archiveArtifacts allowEmptyArchive: true, artifacts: '**/dependency-check-report.html', onlyIfSuccessful: true
-            }
-        }
+        
 
         stage('Maven Deploy') {
             when {
