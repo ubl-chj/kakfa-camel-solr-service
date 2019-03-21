160edde0d80b21471b8bb2a7d1a4434c8b6a1350
Konrad Eichstaedt <konrad.eichstaedt@sbb.sbk-berlin.de>
2019-03-21 09:17:56 +0100

    Enable AutoUpdate for OWASP

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -37,7 +37,18 @@ pipeline {
                 }
             }
         }
-        
+
+        stage('Dependency Check') {
+
+            steps {
+
+                dependencyCheckAnalyzer datadir: 'dependency-check-data', includeVulnReports: true, hintsFile: '', includeCsvReports: false, includeHtmlReports: true, includeJsonReports: false, isAutoupdateDisabled: true, outdir: '', scanpath: '', skipOnScmChange: false, skipOnUpstreamChange: false, suppressionFile: '', zipExtensions: ''
+
+                dependencyCheckPublisher canComputeNew: false, defaultEncoding: '', healthy: '', pattern: '', unHealthy: ''
+
+                archiveArtifacts allowEmptyArchive: true, artifacts: '**/dependency-check-report.html', onlyIfSuccessful: true
+            }
+        }
 
         stage('Maven Deploy') {
             when {
